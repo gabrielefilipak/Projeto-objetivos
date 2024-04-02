cf74876767f1accc08b40594f1fee48ae5656e3c
gabrielefilipak <130515783+gabrielefilipak@users.noreply.github.com>
2024-04-02 09:39:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,11 +15,18 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores= document.querySelectorAll(".contador");
-const tempObjetivo = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempObjetivo);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3.tempoObjetivo4];
 
-function calculaTempo(tempObjetivo) {
+for(let i=0; i<contadores.length;i++){
+    contadores[i].textContent= calculaTempo(tempos[i]);
+}
+
+function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
